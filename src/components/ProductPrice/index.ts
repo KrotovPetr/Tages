6924d7189c6cd1519a8a4d287b86aa8948ae06ec
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2024-07-30 18:19:58 +0300

    /

--- src/components/ProductPrice/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ProductPrice } from './ProductPrice.vue'
--- src/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CartIcon } from './CartIcon.vue'
-export { default as FavouriteIcon } from './FavouriteIcon.vue'
-export { default as InCartIcon } from './InCartIcon.vue'
-export { default as MarkedFavouriteIcon } from './MarkedFavouriteIcon.vue'
